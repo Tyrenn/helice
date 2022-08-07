dfc22187ced2386f0c90aaa9eb468d4162fab1b3
Tyren <guillaume.vailland@bluemarket.blue>
2022-08-08 00:41:39 +0200

    Bien avancée

--- src/select.ts
+++ src/select.ts
@@ -1,15 +1,17 @@
-import { DBFilter, PostgreQueryFilter, QueryBuilder, Stringed, PrefixObject } from './types';
+import { QueryEnvironment, Join, TypedJoin, ExpandEnv, Where, InferAlias, OrderBy, GroupBy } from './types';
 
 
 
-export class SelectQueryBuilder<Env extends TableMap>{
+export class SelectQueryBuilder<Env extends QueryEnvironment>{
 
 	#tablename : string;
-	#fields? : { [key : string] : string};
-	#filters : Array<PostgreQueryFilter<Partial<Schema>>> = [];
+	#joins : Array<{[key : string] : string}> = [];
+	#aliases : Array<{[key : string] : string}> = [];
+	#wheres : Array<{[key : string] : any}> = [];
+	#groupby : Array<string> = [];
+	#orderby : Array<{[key : string] : string}> = [];
 	#limit? : number;
-	#offset? : number;
-	#withs : {alias : string, query : QueryBuilder<any, any, any>}[] = [];
+	#fetch? : number;
 
 	constructor(tablename : string){
 		this.#tablename = tablename; // Or a querybuilder ?
@@ -20,37 +22,38 @@ export class SelectQueryBuilder<Env extends TableMap>{
 		throw new Error('Method not implemented.');
 	}
 
-	limit(l : number) : SelectQueryBuilder<Schema, Fields, Tablename>{
-		this.#limit = l;
+	limit(limit : number){
+		this.#limit = limit;
 		return this;
 	}
 
-	where(f : DBFilter<Schema>) : SelectQueryBuilder<Schema, Fields, Tablename>{
-		this.#filters = Array.isArray(f) ? [...this.#filters, ...f] : [...this.#filters, f];
+	fetch(fetch : number){
+		this.#fetch = fetch;
 		return this;
 	}
 
-	fields<F extends Fields,>(f : F) : SelectQueryBuilder<Schema, F, Tablename>{
-		this.#fields = f;
+	groupby(groupby : GroupBy<Env>){
+		this.#groupby = [...this.#groupby, ...groupby];
 		return this;
 	}
 
-	with<Alias extends string, OF,>(alias : Alias, query : QueryBuilder<any, OF, Alias, any>) : SelectQueryBuilder<Schema, Fields, Tablename, With & { Alias : OF }>{
-		this.#withs.push({alias, query});
+	orderby(orderby : OrderBy<Env>){
+		this.#orderby = [...this.#orderby, ...this.#orderby];
 		return this;
 	}
 
-	from<FromName extends string,>(fromname : FromName) {
-
+	where(where : Where<Env>){
+		return this;
 	}
-/*
-	join<T extends string, C extends string, A extends string,>(type : string, joins : Join<Tablenames | A, Columns, T, C>, alias :  { [T in Tablenames] : A}) : SelectQueryBuilder<Tablenames | A | T, Columns | C>
-	join<T extends string, C extends string>(type : string, joins : Join<Tablenames, Columns, T, C>) : SelectQueryBuilder<Tablenames | T, Columns | C>
-	join<T extends string, C extends string, A extends string,>(type : string, joins : Join<Tablenames | A | T, Columns, T, C>, alias? : { [T in Tablenames] : A}) : SelectQueryBuilder<Tablenames | A | T, Columns | C>{
-		return this as SelectQueryBuilder<Tablenames | A | T, Columns | C>;
-	}*/
 
-	join(joins : Join<Env>, type : string = 'inner'){
+	alias<A extends {[key : string] : string}>(alias : Readonly<A>) : SelectQueryBuilder<ExpandEnv<Env, InferAlias<A>>>{
+		return this;
+	}
+
+	join(joins : Join<Env>) : SelectQueryBuilder<Env>;
+	join(joins : Join<Env>, type : string) : SelectQueryBuilder<Env>;
+	join<A extends {[key : string] : string}>(joins : Join<Env>, type : string, alias : Readonly<A>) : SelectQueryBuilder<ExpandEnv<Env, InferAlias<A>>>;
+	join<A extends {[key : string] : string}>(joins : Join<Env>, type? : string, alias? : Readonly<A>) : SelectQueryBuilder<ExpandEnv<Env, InferAlias<A>>>{
 		return this;
 	}
 
@@ -67,6 +70,7 @@ export class SelectQueryBuilder<Env extends TableMap>{
 	}
 }
 
+
 type Table1 = {
 	column1 : string;
 	column2 : number;
@@ -77,49 +81,8 @@ interface Table2{
 	column22 : string;
 }
 
-let test1 : SelectQueryBuilder<FlattenNestedTableMap<{table1 : Table1, table2 : Table2}>> = new SelectQueryBuilder('big');
+let test1 : SelectQueryBuilder<{table1 : Table1, table2 : Table2}> = new SelectQueryBuilder('big');
 
-let test2 = test1.join({"i:table1:column2" : 'table2:column21'});
+let test2 = test1.alias({'alias1' : 'table1', 'alias2' : 'table2'});
 
-//let teest = test2.join('e', );
-
-//type essai = keyof typeof test3;
-//type essai2 = typeof test3[essai];
-
-// env({EEE : 'ee' | 'ee'})
-
-type ExtractPropsKey<T, TProps extends T[keyof T]> = {
-	[P in keyof T]: T[P] extends TProps ? P : never;
-}[keyof T];
-
-type ExtractTablesKey<T, Tables extends string> = {
-	[P in keyof T] : P extends `${Tables}:${string}` ? P : never;
-}[keyof T]
-
-type ExcludeTables<T, Tables extends string> = Omit<T, ExtractTablesKey<T, Tables>>;
-
-type ExtractTables<T, Tables extends string> = Pick<T, ExtractTablesKey<T, Tables>>;
-
-type ExtractProps<T, TProps extends T[keyof T]> = Pick<T, ExtractPropsKey<T, TProps>>;
-
-type ExtractNestedPropKeys<CO extends {[key : string] : {[key : string] : any}}> = keyof {
-	[Key in (keyof CO) as Key extends string ? `${Key}:${keyof CO[Key] extends string ? keyof CO[Key] : never}` : never] : string;
-}
-
-type SameType<Type extends {[key : string] : any}> = {
-	[Key in keyof Type]? : ExtractPropsKey<Type, Type[Key]>
-}
-
-type SameTypeDifferentTable<Type extends {[key : string] : any}> = {
-	[Key in keyof Type]? : Key extends `${infer U}:${string}` ? Exclude<ExtractPropsKey<Type, Type[Key]>, `${U}:${string}`> : never
-};
-
-type Join<Env extends TableMap> = PrefixObject<SameTypeDifferentTable<Env>, 'i:' | '' | 'l:' | 'r:'>;
-type TypedJoin<Env extends TableMap> = SameTypeDifferentTable<Env>;
-
-
-type FlattenNestedTableMap<Nested extends {[key : string] : {[key : string] : any}}> = {
-	[Key in ExtractNestedPropKeys<Nested>] : Key extends `${infer T}:${infer C}` ? Nested[T][C] : never;
-}
-
-type TableMap = { [key : string] : any};
+let test4 = test2.join({"alias1.column1"});	
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,7 @@
+
+type tablecolumnseparator = '.';
+type modifierseparator = ':';
+
 export type Obj = {
 	[column: string]: any;
 }
@@ -17,36 +21,19 @@ export type PrefixObject<T, P extends string> = {
 }
 
 
+type ArrayedDottedPrefix<Prefix> = Prefix extends string ? `${Prefix}${modifierseparator}` | `[${Prefix}]${modifierseparator}` : never;
 
-/**
- * Type for PostgreSQLQueryFilter
- */
+export type Where<Env extends QueryEnvironment> = Partial<PrefixObject<Arrayed<FlattenQueryEnvironment<Env>>, '' | `[]${modifierseparator}` | ArrayedDottedPrefix<'=' | '<>' | '!=' | '>' | '>=' | '<' | '<=' | '~~' | '~~*' | '!~~' | '!~~*'>>> | Partial<PrefixObject<Arrayed<FlattenQueryEnvironment<Env>>, '' | '[]:' | ArrayedDottedPrefix<'=' | '<>' | '!=' | '>' | '>=' | '<' | '<=' | '~~' | '~~*' | '!~~' | '!~~*'>>>[];
 
-type prefixdotted<T, P extends string> = {
-	[K in keyof T as K extends string ? `${P}:${K}` : never]: T[K]
+
+export type InferAlias<A extends {[key : string] : string}> = {[key in keyof A] : A[key] extends `${infer U}` ? U : never};
+
+
+export type OrderBy<Env extends QueryEnvironment> = {
+	[Key in keyof FlattenQueryEnvironment<Env>]? : 'ASC' | 'DESC' | '';
 }
 
-type arrayprefixdotted<T, P extends string> = {
-	[K in keyof T as K extends string ? `[${P}]:${K}` : never]: T[K]
-}
-
-type pc<T, P extends string> = prefixdotted<Arrayed<T>,P> & arrayprefixdotted<Arrayed<T>, P>;
-
-export type PostgreQueryFilter<T> = Arrayed<T> & 
-	pc<T, ''> & 
-	pc<T, '='> &
-	pc<T, '<>'> &
-	pc<T, '!='> &
-	pc<T, '>'> &
-	pc<T, '>='> &
-	pc<T, '<'> &
-	pc<T, '<='> &
-	pc<T, '~~'> &
-	pc<T, '~~*'> &
-	pc<T, '!~~'> &
-	pc<T, '!~~*'>
-
-
+export type GroupBy<Env extends QueryEnvironment> = (keyof FlattenQueryEnvironment<Env> extends string ? keyof FlattenQueryEnvironment<Env> : never)[];	
 
 /**
  * Transform every properties of an object to string if not object
@@ -56,10 +43,97 @@ export type Stringed<T> = {
 }
 
 
-export type DBFilter<O> = PostgreQueryFilter<Partial<O>> | PostgreQueryFilter<Partial<O>>[];
-
+//export type DBFilter<O> = PostgreQueryFilter<Partial<O>> | PostgreQueryFilter<Partial<O>>[];
 
 
 export interface QueryBuilder<Schema, Fields, Tablename>{
 	quild() : { text : string, values : any[], nbvalues : number};
 }
+
+
+export type ExtractPropsKey<T, TProps extends T[keyof T]> = {
+	[P in keyof T]: T[P] extends TProps ? P : never;
+}[keyof T];
+
+export type ExtractTablesKey<T, Tables extends string> = {
+	[P in keyof T] : P extends `${Tables}${tablecolumnseparator}${string}` ? P : never;
+}[keyof T]
+
+export type ExcludeTables<T, Tables extends string> = Omit<T, ExtractTablesKey<T, Tables>>;
+
+export type ExtractTables<T, Tables extends string> = Pick<T, ExtractTablesKey<T, Tables>>;
+
+export type ExtractProps<T, TProps extends T[keyof T]> = Pick<T, ExtractPropsKey<T, TProps>>;
+
+export type ExtractNestedPropKeys<CO extends {[key : string] : {[key : string] : any}}> = keyof {
+	[Key in (keyof CO) as Key extends string ? `${Key}${tablecolumnseparator}${keyof CO[Key] extends string ? keyof CO[Key] : never}` : never] : string;
+}
+
+export type SameType<Type extends {[key : string] : any}> = {
+	[Key in keyof Type]? : ExtractPropsKey<Type, Type[Key]>
+}
+
+export type SameTypeDifferentTable<Type extends {[key : string] : any}> = {
+	[Key in keyof Type]? : Key extends `${infer U}${tablecolumnseparator}${string}` ? Exclude<ExtractPropsKey<Type, Type[Key]>, `${U}${tablecolumnseparator}${string}`> : never
+};
+
+export type FlattenQueryEnvironment<Nested extends QueryEnvironment> = {
+	[Key in ExtractNestedPropKeys<Nested>] : Key extends `${infer T}${tablecolumnseparator}${infer C}` ? Nested[T][C] : never;
+}
+
+export type ExpandEnv<Env extends QueryEnvironment, Alias extends {[key : string] : string}> = {
+	[Key in (keyof Env) | (keyof Alias)] : Key extends (keyof Alias) ? Env[Alias[Key]] : Env[Key extends string ? Key : never];
+}
+
+export type Join<Env extends QueryEnvironment> = PrefixObject<SameTypeDifferentTable<FlattenQueryEnvironment<Env>>, `i${modifierseparator}` | '' | `l${modifierseparator}` | `r${modifierseparator}`>;
+export type TypedJoin<Env extends QueryEnvironment> = SameTypeDifferentTable<FlattenQueryEnvironment<Env>>;
+
+
+type Table1 = {
+	column1 : string;
+	column2 : number;
+	column4 : number;
+	column5 : string[];
+	column6 : 'EEE';
+	column7 : boolean;
+}
+
+interface Table2{
+	column21 : number;
+	column22 : string;
+}
+
+type testenv = {
+	table1 : Table1;
+	table2 : Table2;
+}
+
+type alias = {
+	alias1 : "table1";
+}
+
+
+/*
+	Hacky things from 
+	https://stackoverflow.com/questions/52855145/typescript-object-type-to-array-type-tuple 
+	https://stackoverflow.com/questions/55127004/how-to-transform-union-type-to-tuple-type/55128956#55128956
+*/
+type UnionToIntersection<U> =
+	(U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never
+
+type LastOf<T> =
+	UnionToIntersection<T extends any ? () => T : never> extends () => (infer R) ? R : never
+
+// TS4.0+
+type Push<T extends any[], V> = [...T, V];
+
+// TS4.1+
+type TuplifyUnion<T, L = LastOf<T>, N = [T] extends [never] ? true : false> =
+	true extends N ? [] : Push<TuplifyUnion<Exclude<T, L>>, L>
+
+type ObjValueTuple<T, KS extends any[] = TuplifyUnion<keyof T>, R extends any[] = []> =
+	KS extends [infer K, ...infer KT]
+	? ObjValueTuple<T, KT, [...R, T[K & keyof T]]>
+	: R
+
+export type QueryEnvironment = { [key : string] : {[key : string] : any}};
