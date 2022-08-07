008b86ed4ae462a2de24d0022a1f6a948d135b8c
Tyren <guillaume.vailland@bluemarket.blue>
2022-08-07 14:19:33 +0200

    Type d'environnement

--- src/select.ts
+++ src/select.ts
@@ -1,8 +1,8 @@
-import { DBFilter, PostgreQueryFilter, QueryBuilder, Stringed } from './types';
+import { DBFilter, PostgreQueryFilter, QueryBuilder, Stringed, PrefixObject } from './types';
 
 
 
-export class SelectQueryBuilder<Tablenames extends string, Columns extends string>{
+export class SelectQueryBuilder<Env extends TableMap>{
 
 	#tablename : string;
 	#fields? : { [key : string] : string};
@@ -43,88 +43,83 @@ export class SelectQueryBuilder<Tablenames extends string, Columns extends strin
 	from<FromName extends string,>(fromname : FromName) {
 
 	}
-
+/*
 	join<T extends string, C extends string, A extends string,>(type : string, joins : Join<Tablenames | A, Columns, T, C>, alias :  { [T in Tablenames] : A}) : SelectQueryBuilder<Tablenames | A | T, Columns | C>
 	join<T extends string, C extends string>(type : string, joins : Join<Tablenames, Columns, T, C>) : SelectQueryBuilder<Tablenames | T, Columns | C>
-	join<T extends string, C extends string, A extends string,>(type : string, joins : Join<Tablenames | A, Columns, T, C>, alias? : { [T in Tablenames] : A}) : SelectQueryBuilder<Tablenames | A | T, Columns | C>{
+	join<T extends string, C extends string, A extends string,>(type : string, joins : Join<Tablenames | A | T, Columns, T, C>, alias? : { [T in Tablenames] : A}) : SelectQueryBuilder<Tablenames | A | T, Columns | C>{
 		return this as SelectQueryBuilder<Tablenames | A | T, Columns | C>;
+	}*/
+
+	join(joins : Join<Env>, type : string = 'inner'){
+		return this;
+	}
+
+	innerjoin(joins : TypedJoin<Env>){
+		return this;
+	}
+
+	leftjoin(joins : TypedJoin<Env>){
+		return this;
+	}
+
+	rightjoin(joins : TypedJoin<Env>){
+		return this;
 	}
 }
 
-let test1 : SelectQueryBuilder<"Basic", 'Coucou'> = new SelectQueryBuilder('big');
-
-let test2 = test1.join('r', {'Basic:Coucou' : 'a:e', 'b:Coucou'}, {'Basic' : 'b'});
-
-let teest = test2.join('e', );
-
-export type PrefixObject<T, P extends string> = {
-	[K in keyof T as K extends string ? `${P}${K}` : never]: T[K]
+type Table1 = {
+	column1 : string;
+	column2 : number;
 }
 
-type toArray<T extends string> =  Array<T>;
-
-let teeest : toArray<'a' | 'b'> = ['a', 'b'];
-
-let test3 : Readonly<Join<'a' | 'b', 'c' | 'd', string, string, string>> = { "a:c" : "aa:a"};
-
-type essai = keyof typeof test3;
-type essai2 = typeof test3[essai];
-
-type left<T> = T extends `${infer U}:${string}` ? U : never;
-type right<T> = T extends `${string}:${infer U}` ? U : never;
-
-let x : right<"a:e"> = 'e';
-
-type condtype<T> =  T extends string ? `${T}` : never;
-type containing<Left extends string, Right extends string> = Left extends string ? `${Left}:${Right}` : never;
-
-type Join<Tablenames extends string, Columns extends string, OtherT extends `${string}`, OtherC extends string> = {
-	[T in Tablenames as T extends string ? `${T}:${Columns}` : never]? : `${OtherT}:${OtherC}`;
+interface Table2{
+	column21 : number;
+	column22 : string;
 }
 
+let test1 : SelectQueryBuilder<FlattenNestedTableMap<{table1 : Table1, table2 : Table2}>> = new SelectQueryBuilder('big');
 
-type expandingself<Keys extends string, Values extends string,> = {
-	[K in (Keys | Values)]? : Values;
+let test2 = test1.join({"i:table1:column2" : 'table2:column21'});
+
+//let teest = test2.join('e', );
+
+//type essai = keyof typeof test3;
+//type essai2 = typeof test3[essai];
+
+// env({EEE : 'ee' | 'ee'})
+
+type ExtractPropsKey<T, TProps extends T[keyof T]> = {
+	[P in keyof T]: T[P] extends TProps ? P : never;
+}[keyof T];
+
+type ExtractTablesKey<T, Tables extends string> = {
+	[P in keyof T] : P extends `${Tables}:${string}` ? P : never;
+}[keyof T]
+
+type ExcludeTables<T, Tables extends string> = Omit<T, ExtractTablesKey<T, Tables>>;
+
+type ExtractTables<T, Tables extends string> = Pick<T, ExtractTablesKey<T, Tables>>;
+
+type ExtractProps<T, TProps extends T[keyof T]> = Pick<T, ExtractPropsKey<T, TProps>>;
+
+type ExtractNestedPropKeys<CO extends {[key : string] : {[key : string] : any}}> = keyof {
+	[Key in (keyof CO) as Key extends string ? `${Key}:${keyof CO[Key] extends string ? keyof CO[Key] : never}` : never] : string;
 }
 
-let y : expandingself<'k', 'v'> = {
-	'k' : 'v',
-	'v' : 'v'
+type SameType<Type extends {[key : string] : any}> = {
+	[Key in keyof Type]? : ExtractPropsKey<Type, Type[Key]>
 }
 
-function lel<T extends string>(y : expandingself<'k', T>){
+type SameTypeDifferentTable<Type extends {[key : string] : any}> = {
+	[Key in keyof Type]? : Key extends `${infer U}:${string}` ? Exclude<ExtractPropsKey<Type, Type[Key]>, `${U}:${string}`> : never
+};
 
+type Join<Env extends TableMap> = PrefixObject<SameTypeDifferentTable<Env>, 'i:' | '' | 'l:' | 'r:'>;
+type TypedJoin<Env extends TableMap> = SameTypeDifferentTable<Env>;
+
+
+type FlattenNestedTableMap<Nested extends {[key : string] : {[key : string] : any}}> = {
+	[Key in ExtractNestedPropKeys<Nested>] : Key extends `${infer T}:${infer C}` ? Nested[T][C] : never;
 }
 
-lel({'k' : 'c', 'c' : 'aa', 'aa' : 'b'})
-
-type Alias<Tablenames extends string> = {
-	[T in Tablenames] : string;
-}
-
-function toJOINClause(join : obj[]){
-
-}
-
-/*
-* WITH should be a query wrapper with fields, alias and name.
-* Different types of join.
-* Each table can have an alias
-* Each join has an on with alias names and columne name... alias1.column1 = alias2.column2
-* Join as { type, tablenamefrom, aliasfrom?, columnfrom, tablenameto, columnto}
-*
-* Propositions : 
-* 	{ type:tablename alias : 'column' } ? Missing the tablenameto/columnto, alias not handy...
-* 	{ type:tablename : { alias : '', from...}} Boilerplate...
-*  { tablenamefrom:columnfrom : tablenameto:columnto }
-* [type]join(type, [{tablenamefrom : columnto}, {tablenameto : columnto}])
-* alias method
-* 
-* from can be simple string or
-*  { 'i:tablename' : 'one.id'}
-* select(from)
-*
-*
-*
-
-*/
+type TableMap = { [key : string] : any};
